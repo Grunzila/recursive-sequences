0856a293ac660a238bf62d0560e65fecd8a3e55b
Paul Roylance <grunzila@gmail.com>
2017-03-12 04:06:52 +0000

    working recursive functions of fibonacci, pell, and tribonacci

--- scratchpad.js
+++ scratchpad.js
@@ -1,3 +1,18 @@
-var fibBox
-var pellBox
-var tribBoc
+function fibonacci(num){
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+function pell(num){
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    return 2*pell(num - 1) + pell(num - 2);
+}
+
+function tribonacci(num){
+    if (num === 0) return 0;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+    return tribonacci(num - 1) + tribonacci(num - 2) + tribonacci(num - 3);
+}
