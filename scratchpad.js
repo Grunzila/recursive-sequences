6846dfccc5f52d1fe3d71a2476e1dce9ff774f03
Paul Roylance <grunzila@gmail.com>
2017-03-14 01:01:22 +0000

    Added function to create the text of elements, and adapted fibonacci styling and function to the pell sequence

--- scratchpad.js
+++ scratchpad.js
@@ -1,52 +1,6 @@
 document.title = 'Fabulous Number Sequence Trees!';
 document.body.style.backgroundColor = '#B09090';
 
-function fibonacci(num, node){
-    var sum;
-    var div = divBuild('fib', null);
-    if (num <= 1){
-        if (num === 0) sum = 0;
-        else if (num === 1) sum = 1;
-        
-        var p = document.createElement('p');
-        p.textContent = 'Fib(' + num + ') = ' + sum;
-        div.appendChild(p);
-    }
-    else {
-        var left = fibonacci(num - 1, div);
-        var className = left.node.getAttribute("class");
-        left.node.setAttribute('class', className + ' left');
-        
-        var right = fibonacci(num - 2, div);
-        className = right.node.getAttribute("class");
-        right.node.setAttribute('class', className + ' right');
-        
-        sum = left.result + right.result;
-        //console.log('(' + num + ')[' + sum + '];');
-        
-        var p = document.createElement('p');
-        p.textContent = 'Fib(' + num + ') = ' + sum;
-        div.appendChild(p);
-        
-        div.appendChild(left.node);
-        div.appendChild(right.node);
-    }
-    return {'result': sum, 'node': div};
-}
-
-function pell(num, node){
-    if (num === 0) return 0;
-    if (num === 1) return 1;
-    return 2*pell(num - 1) + pell(num - 2);
-}
-
-function tribonacci(num){
-    if (num === 0) return 0;
-    if (num === 1) return 0;
-    if (num === 2) return 1;
-    return tribonacci(num - 1) + tribonacci(num - 2) + tribonacci(num - 3);
-}
-
 function divBuild(name, id){
     var div = document.createElement('div');
     div.setAttribute('class', name);
@@ -54,6 +8,75 @@ function divBuild(name, id){
         div.setAttribute('id', id);
     document.body.appendChild(div);
     return div;
+}
+
+function pSeqBuild(name, iter, sum, father){
+    var p = document.createElement('p');
+    p.textContent = name + '(' + iter + ') = ' + sum;
+    father.appendChild(p);
+}
+
+function fibonacci(iter, node){
+    var sum;
+    var div = divBuild('fib', null);
+    if (iter <= 1){
+        if (iter === 0) sum = 0;
+        else if (iter === 1) sum = 1;
+        
+        pSeqBuild('Fib', iter, sum, div);
+    }
+    else {
+        var left = fibonacci(iter - 1, div);
+        var className = left.node.getAttribute("class");
+        left.node.setAttribute('class', className + ' left');
+        
+        var right = fibonacci(iter - 2, div);
+        className = right.node.getAttribute("class");
+        right.node.setAttribute('class', className + ' right');
+        
+        sum = left.result + right.result;
+        
+        pSeqBuild('Fib', iter, sum, div);
+        
+        div.appendChild(left.node);
+        div.appendChild(right.node);
+    }
+    return {'result': sum, 'node': div};
+}
+
+function pell(iter, node){
+    var sum;
+    var div = divBuild('pell', null);
+    if (iter <= 1){
+        if (iter === 0) sum = 0;
+        else if (iter === 1) sum = 1;
+        
+        pSeqBuild('Pell', iter, sum, div);
+    }
+    else {
+        var left = pell(iter - 1, div);
+        var className = left.node.getAttribute("class");
+        left.node.setAttribute('class', className + ' left');
+        
+        var right = pell(iter - 2, div);
+        className = right.node.getAttribute("class");
+        right.node.setAttribute('class', className + ' right');
+        
+        sum = 2*left.result + right.result;
+        
+        pSeqBuild('Pell', iter, sum, div);
+        
+        div.appendChild(left.node);
+        div.appendChild(right.node);
+    }
+    return {'result': sum, 'node': div};
+}
+
+function tribonacci(num){
+    if (num === 0) return 0;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+    return tribonacci(num - 1) + tribonacci(num - 2) + tribonacci(num - 3);
 }
 
 function linkBuild(link, text){
@@ -66,20 +89,16 @@ function linkBuild(link, text){
     document.body.appendChild(a);
 }
 
+
+
 var fibo = divBuild('fibonacci box', 'fibonacci');
-var pell = divBuild('pell box', 'pell');
+var pello = divBuild('pell box', 'pell');
 var tribo = divBuild('tribonacci box', 'tribonacci');
 
 fibo.appendChild(fibonacci(5, fibo).node);
-//pell.appendChild(pell(5, pell).node);
+pello.appendChild(pell(5, pello).node);
 //tribo.appendChild(tribo(5, tribo).node);
 
-fibo.style.background = 'rgba(200,50,0,0.5)';
-fibo.style.border = '5px solid rgba(210,30,0,0.7)';
-pell.style.background = 'rgba(175,75,0,0.5)';
-pell.style.border = '5px solid rgba(165,85,0,0.7)';
-tribo.style.background = 'rgba(150,100,0,0.5)';
-tribo.style.border = '5px solid rgba(140,110,0,0.7)';
 
 var box = document.querySelectorAll('.box');
 for (var i = 0; i < box.length; i++){
@@ -87,6 +106,27 @@ for (var i = 0; i < box.length; i++){
   box[i].style.width = '900px';
   box[i].style.margin = '20px 5px';
 }
+
+fibo.style.background = 'rgba(200,50,0,0.5)';
+fibo.style.border = '5px solid rgba(210,30,0,0.7)';
+var fib = document.querySelectorAll('.fib');
+for (var i = 0; i < fib.length; i++){
+  fib[i].style.textAlign = 'center';
+  fib[i].style.backgroundColor = 'rgba(255,255,255,0.1)';
+  fib[i].style.display = 'inline-block';
+}
+
+pello.style.background = 'rgba(175,75,0,0.5)';
+pello.style.border = '5px solid rgba(165,85,0,0.7)';
+var pel = document.querySelectorAll('.pell');
+for (var i = 0; i < pel.length; i++){
+  pel[i].style.textAlign = 'center';
+  pel[i].style.backgroundColor = 'rgba(255,255,255,0.1)';
+  pel[i].style.display = 'inline-block';
+}
+
+tribo.style.background = 'rgba(150,100,0,0.5)';
+tribo.style.border = '5px solid rgba(140,110,0,0.7)';
 
 var right = document.querySelectorAll('.right');
 for (var i = 0; i < right.length; i++){
@@ -100,13 +140,6 @@ for (var i = 0; i < left.length; i++){
   //left[i].style.display = 'inline-block';
   left[i].style.float = 'left';
   left[i].style.marginRight = '3px';
-}
-
-var fib = document.querySelectorAll('.fib');
-for (var i = 0; i < fib.length; i++){
-  fib[i].style.textAlign = 'center';
-  fib[i].style.backgroundColor = 'rgba(200,200,200,0.15)';
-  fib[i].style.display = 'inline-block';
 }
 
 linkBuild('https://oeis.org/A000045', 'Fibonacci Sequence|');
